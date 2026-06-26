c7d4d20dc00ba69829af5b50f286e9c8764065a9
Noeiibibi <159039367+Noeiibibi@users.noreply.github.com>
2026-06-26 08:32:45 +0700

    Refactor: cleanup repo structure, remove redundant training scripts, and organize docs

--- src/train_lgbm.py
+++
@@ -1,9 +0,0 @@
-from lightgbm import LGBMRegressor
-
-from config import LGBM_PARAMS
-
-
-def train_lgbm_model(X_train, y_train):
-    model = LGBMRegressor(**LGBM_PARAMS)
-    model.fit(X_train, y_train)
-    return model
